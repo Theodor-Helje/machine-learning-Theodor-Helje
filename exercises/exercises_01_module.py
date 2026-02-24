2bc0aa3b09e2c18c17d41594ad4b4e00cba0fd6e
Theodor Helje <Theodor.Helje@proton.me>
2026-02-24 17:02:58 +0100

    work on exercises_01

---
+++ exercises/exercises_01_module.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
+#from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+def fit_and_eval(X_train, X_test, y_train, y_test, model):
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mae = mean_absolute_error(y_true=y_test, y_pred=y_pred)
+    mse = mean_squared_error(y_true=y_test, y_pred=y_pred)
+    rmse = root_mean_squared_error(y_true=y_test, y_pred=y_pred)
+
+    return {'mae': mae, 
+            'mse': mse, 
+            'rmse': rmse}
