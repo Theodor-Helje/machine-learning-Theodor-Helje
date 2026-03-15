c1c6db3575f139915e68097c1a381c216621777d
Theodor Helje <Theodor.Helje@proton.me>
2026-03-15 15:00:45 +0100

    fixed matrix format, changed all function to csr format

--- Labb-1/pipeline.py
+++ Labb-1/pipeline.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, hstack
 from scipy.sparse import save_npz, load_npz
 
 
@@ -20,41 +20,38 @@ def dummy_encode_movies():
     movies['genres'] = movies['genres'].str.split('|')
 
     mlb = MultiLabelBinarizer()
-    movies_encoded = movies.join(pd.DataFrame(mlb.fit_transform(movies['genres']), columns=mlb.classes_))
+    movies_matrix = mlb.fit_transform(movies['genres'])
 
-    return movies_encoded.drop(['genres'], axis='columns')
+    movie_sorting = movies['movieId'].map(get_mapping_dicts()[0]).argsort()
+
+    return csr_matrix(movies_matrix[movie_sorting])
 
 
 def tfidf_encode_tags():
     tags = pd.read_csv('Labb-1/ml-latest/tags.csv')
-    tags_tfidf = tags.dropna(axis=0).groupby('movieId')['tag'].apply(lambda x: ' '.join(x).lower())
+    tags_grouped = tags.dropna(axis=0).groupby('movieId')['tag'].apply(lambda x: ' '.join(x).lower())
+    mapping = get_mapping_dicts()[0]
 
     tfidf_vectorizer = TfidfVectorizer(max_features=5000, stop_words='english')
-    tag_vectorized_matrix = tfidf_vectorizer.fit_transform(tags_tfidf)
+    tfidf_matrix = tfidf_vectorizer.fit_transform(tags_grouped)
 
-    tags_vectorized = pd.DataFrame.sparse.from_spmatrix(
-        tag_vectorized_matrix, 
-        index=tags_tfidf.index, 
-        columns=tfidf_vectorizer.get_feature_names_out()
-    ).fillna(0)
+    tags_sorting = tags_grouped.index.map(mapping)
 
-    return tags_vectorized
+    tags_sparse = csr_matrix(
+        (tfidf_matrix.data,
+         (tags_sorting[tfidf_matrix.nonzero()[0]],
+         tfidf_matrix.nonzero()[1])),
+         shape=(len(mapping), tfidf_matrix.shape[1])
+    )
+
+    return tags_sparse
 
 
 def get_movie_features():
-    movie_features = pd.concat(
-        [dummy_encode_movies().set_index(['movieId']), tfidf_encode_tags()],
-        axis=1
-    ).fillna(0)
-    
-    movie_features.index = movie_features.index.map(get_mapping_dicts()[0])
-    movie_features.index.name = 'movieId_mapped'
-    
-    movie_features = movie_features.sort_index()
+    genre_matrix = dummy_encode_movies()
+    tags_matrix = tfidf_encode_tags()
 
-    movie_features = movie_features.astype(pd.SparseDtype('float', 0))
-
-    return movie_features.sparse.to_coo().to_csr()
+    return hstack([genre_matrix, tags_matrix]).tocsr()
 
 
 def get_user_interaction_matrix():
@@ -73,10 +70,10 @@ def get_user_interaction_matrix():
 
 
 if __name__ == "__main__":
-    print('creating movies encoding')
+    print('creating movies encoded matrix')
     movies_encoded = dummy_encode_movies()
 
-    print('creating tags tf-idf encoding')
+    print('creating tags tf-idf encoded matrix')
     tags_encoded = tfidf_encode_tags()
 
     print('creating movie features matrix')
@@ -85,13 +82,11 @@ if __name__ == "__main__":
     print("creating user interaction matrix")
     interaction_matrix = get_user_interaction_matrix()
 
-    print("saving matrices")
-    save_npz('Labb-1/ml-latest/interaction_matrix.npz', get_user_interaction_matrix())
-    save_npz('Labb-1/ml-latest/movie_feature_matrix.npz', get_movie_features())
+    print("saving matrices\n")
+    save_npz('Labb-1/ml-latest/interaction_matrix.npz', interaction_matrix)
+    save_npz('Labb-1/ml-latest/movie_feature_matrix.npz', movie_features)
 
-    print(f"dummy encoded movies:\n{movies_encoded}\n")
-    print(f"tfidf encoded tags:\n{tags_encoded}\n")
-    print(f"combined DataFrame:\n{movie_features}\n")
-
-    print(f"movie features matrix shape:{movie_features.shape}\n")
-    print(f"interaction matrix shape: {interaction_matrix.shape}")
+    print(f"dummy encoded movies matrix shape: {movies_encoded.shape}\n")
+    print(f"tfidf encoded tags matrix shape: {tags_encoded.shape}\n")
+    print(f"movie features matrix shape: {movie_features.shape}\n")
+    print(f"interaction matrix shape: {interaction_matrix.shape}\n")
