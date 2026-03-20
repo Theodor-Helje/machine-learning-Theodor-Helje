5c98109f4f8c29c74f84d5c112ed6dc8b0306a20
Theodor Helje <Theodor.Helje@proton.me>
2026-03-20 17:10:50 +0100

    work on implementing functions to recognise data state changes

--- Labb-1/data.py
+++ Labb-1/data.py
@@ -2,11 +2,12 @@ import pandas as pd
 import os
 import yaml
 from scipy.sparse import save_npz, load_npz
+from scipy.sparse import csr_matrix
+import numpy as np
 
 
-def load_file(file="movies", path=None):
-    if path is None:
-        default_paths = {
+def get_default_paths():
+    default_paths = {
             "movies": "Labb-1/data-files/ml-latest/movies.csv",
             "ratings": "Labb-1/data-files/ml-latest/ratings.csv",
             "tags": "Labb-1/data-files/ml-latest/tags.csv",
@@ -14,13 +15,21 @@ def load_file(file="movies", path=None):
             "interaction_matrix": "Labb-1/data-files/matrices/interaction_matrix.npz",
             "hyperparameters": "Labb-1/hyperparameters.yml"
         }
+    return default_paths
+
+
+def load_file(file="movies", path=None):
+    if path is None:
+        default_paths = get_default_paths()
         path = default_paths[file]
 
     if os.path.exists(path):
         if path[-1] == "v":
             return pd.read_csv(path)
         elif path[-1] == "l":
-            return yaml.safe_load(path)
+            with open(path, 'r') as file:
+                hyperparameters = yaml.safe_load(file)["reccomender"]
+            return hyperparameters
         else:
              return load_npz(path)
     else:
@@ -30,14 +39,52 @@ def load_file(file="movies", path=None):
 def save_file(file_name, file):
     if file_name[-1] == "v":
         os.makedirs("Labb-1/data-files/csv_files", exist_ok=True)
-        file.to_csv(f"Labb-1/data-files/csv_files/{file_name}")
-    else:
+        file.to_csv(f"Labb-1/data-files/csv_files/{file_name}", index=False)
+    elif file_name[-1] == "z":
         os.makedirs("Labb-1/data-files/matrices", exist_ok=True)
         save_npz(f"Labb-1/data-files/matrices/{file_name}", file)
+    else:
+        raise ValueError("files must be saved as .npz or .csv format")
 
+
+def create_state_df(hyperparameters=None):
+    """creates checkpoint to save the state of the model to compare to when starting model"""
+    if hyperparameters is None:
+        hyperparameters = load_file(file="hyperparameters")
+    
+    paths = get_default_paths()
+
+    storage_space = [os.path.getsize(path) for path in paths.values()]
+
+    state = pd.DataFrame(data=[*hyperparameters.values(),
+                               *storage_space])
+    
+    return state
+
+
+def compare_state(current_checkpoint=None):
+    """returns True if current state is equal to stored state"""
+    if current_checkpoint is None:
+        current_checkpoint = create_state_df()
+    
+    try:
+        return np.allclose(current_checkpoint.values, load_file(path="Labb-1/data-files/csv_files/state.csv").values, equal_nan=True)
+    except FileNotFoundError:
+        return False
 
 
 if __name__ == "__main__":
-     print(f"movies:\n{load_file("movies").head()}\n")
-     print(f"ratings:\n{load_file("ratings").head()}\n")
-     print(f"tags:\n{load_file("tags").head()}\n")
+    print(f"hyperparameters:\n{load_file("hyperparameters")}\n")
+    print(f"movies:\n{load_file("movies").head()}\n")
+    print(f"ratings:\n{load_file("ratings").head()}\n")
+    print(f"tags:\n{load_file("tags").head()}\n")
+
+    print("\ncreating state DF\n")
+    state = create_state_df()
+    print(f"checkpoint DF:\n{state}\n")
+
+    print("checking state")
+    print(f"state unchanged: {compare_state(state)}\n")
+
+    print("saving state")
+    save_file("state.csv", state)
