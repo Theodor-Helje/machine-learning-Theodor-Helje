ce141a9160a651700e3ff5f2897feac3cb43d051
Theodor Helje <Theodor.Helje@proton.me>
2026-04-20 15:59:20 +0200

    work on CnnVisualizer class

--- Labb-2/image_classifyer.py
+++ Labb-2/image_classifyer.py
@@ -3,6 +3,8 @@ from torchvision.models import get_model, get_model_weights
 from torchcam.methods import LayerCAM
 from torchvision.transforms.v2.functional import to_pil_image
 from torchcam.utils import overlay_mask
+from torch.nn.functional import softmax
+from torch import topk
 import matplotlib.pyplot as plt
 
 
@@ -13,40 +15,79 @@ class ResnetVisualizer():
         self.preprocess = self.weights.transforms()
         self.labels = self.weights.meta["categories"]
 
+        self.layers = {
+            "1": self.model.layer1,
+            "2": self.model.layer2,
+            "3": self.model.layer3,
+            "4": self.model.layer4
+        }
+
         self.img = None
         self.img_tensor = None
         self.logits = None
-        self.activation_map = None
+        self.layer = None
+        self.top_k_index = None
+        self.top_k_probs = None
+        self.activation_maps = None
 
+    def fit_image(self, img_path, top_k=1, layer=None):
+        """fits an image, saves img, img_tensor, logits and activation maps as properties"""
 
-    def fit_image(self, img_path):
-        """fits an image, saves img, img_tensor, logits and activation map as properties"""
+        if layer == None:
+            self.layer = "4"
+        else:
+            self.layer = str(layer)
+        layer = self.layers[self.layer]
 
         self.img = decode_image(img_path)
         self.img_tensor = self.preprocess(self.img)
 
-        with LayerCAM(self.model, target_layer=self.model.layer4) as cam_extractor:
+        with LayerCAM(self.model, target_layer=layer) as cam_extractor:
             self.logits = self.model(self.img_tensor.unsqueeze(0))
-            self.activation_map = cam_extractor(self.logits.squeeze(0).argmax().item(), self.logits)
+
+            probs = softmax(self.logits, dim=1)
+            top_probs, top_ids = topk(probs, top_k)
+
+            self.top_k_index = top_ids[0]
+            self.top_k_probs = top_probs[0]
+            self.activation_maps = []
+
+            for id in self.top_k_index:
+                self.logits = self.model(self.img_tensor.unsqueeze(0))
+
+                cam = cam_extractor(id.item(), self.logits)
+                self.activation_maps.append(cam[0].squeeze(0))
 
 
-    def visualize_prediction(self):
-        """shows fitted original image with heatmap overlay showing activation"""
+    def visualize_prediction(self, only_top_1=False):
+        """plots top k predictions as heatmaps on the original fitted img"""
 
-        if self.logits is None:
-            raise ValueError("Image must be fitted to visualize")
+        if only_top_1:
+            k = 1
+        else:
+            k = len(self.top_k_index)
 
-        result = overlay_mask(to_pil_image(self.img), to_pil_image(self.activation_map[0].squeeze(0), mode='F'), alpha=0.5)
+        fig, axes = plt.subplots(1, k, figsize=(6 * k, 6))
 
-        plt.imshow(result)
-        plt.axis('off')
+        if k == 1:
+            axes = [axes]
+
+        for i in range(k):
+            class_id = self.top_k_index[i].item()
+            prob = self.top_k_probs[i].item()
+            cam_map = self.activation_maps[i]
+            result = overlay_mask(to_pil_image(self.img), to_pil_image(cam_map, mode='F'), alpha=0.5)
+
+            axes[i].imshow(result)
+            axes[i].axis('off')
+            axes[i].set_title(f"predicted class: {self.labels[class_id]}\nid: {class_id}\nprob: {prob: .3f}\nlayer: {self.layer}")
+
         plt.tight_layout()
-        plt.title(f"predicted class: {self.labels[self.logits.squeeze(0).argmax().item()]}, id: {self.logits.squeeze(0).argmax().item()}")
         plt.show()
 
 
-    def fit_visualize_image(self, img_path):
-        self.fit_image(img_path)
+    def fit_visualize_image(self, img_path, top_k=1, layer=None):
+        self.fit_image(img_path, top_k, layer)
         self.visualize_prediction()
 
 
@@ -54,4 +95,7 @@ class ResnetVisualizer():
 if __name__ == "__main__":
     visualizer = ResnetVisualizer()
 
-    visualizer.fit_visualize_image("data/skivspelare.jpeg")
+    visualizer.fit_visualize_image("data/1990-mercedes-benz-190e-evo-ii.jpg", top_k=3, layer=4)
+
+    for i in [1, 2, 3, 4]:
+        visualizer.fit_visualize_image("data/1990-mercedes-benz-190e-evo-ii.jpg", top_k=3, layer=i)
